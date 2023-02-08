98812fec00c613f13813e0a9290d7358749943e5
Chaewony <tcd1205@naver.com>
2023-02-08 17:19:01 +0900

    firebase 환경설정

---
+++ app/src/main/java/com/eustress/moong/MainActivity.java
@@ -0,0 +1,31 @@
+package com.eustress.moong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        // 활동 초기화시 사용자가 현재 로그인되어 있는지 확인
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+}
