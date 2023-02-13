c876593e3c9c1fe85f642239b600e522b0139b2a
Chaewony <tcd1205@naver.com>
2023-02-13 16:24:16 +0900

    로그인 구현1
    로그인 기능 일부 구현 - 앱 안에서 회원가입 후 로그인 가능
    로그인 UI 개선(잘못된 email을 입력했습니다, 문자열 초과 등) 빨갛게 변하는거 - 일단은 회원가입창만
    기능 추가 - PW 확인
    예외처리 - 회원가입 실패 시 튕기지 않게 하기

--- app/src/main/java/com/eustress/moong/JoinActivity.java
+++ app/src/main/java/com/eustress/moong/JoinActivity.java
@@ -1,13 +1,49 @@
 package com.eustress.moong;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
+
 public class JoinActivity extends AppCompatActivity {
+
+    private Button join;
+    private com.google.android.material.textfield.TextInputEditText name;
+    private com.google.android.material.textfield.TextInputEditText birth;
+    private com.google.android.material.textfield.TextInputEditText email;
+    private com.google.android.material.textfield.TextInputEditText pw;
+    private com.google.android.material.textfield.TextInputEditText pwCheck;
+    FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private com.google.android.material.textfield.TextInputLayout email_layout;
+    private com.google.android.material.textfield.TextInputLayout pw_layout;
+    private com.google.android.material.textfield.TextInputLayout pwCheck_layout;
+
+    private boolean rightEmail = false; //회원가입을 정상적으로 할 수 있는지 판별하는 변수
+    private boolean rightPW = false; //회원가입을 정상적으로 할 수 있는지 판별하는 변수
+    private boolean rightPWCheck = false; //회원가입을 정상적으로 할 수 있는지 판별하는 변수
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +51,165 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        join = (Button) findViewById(R.id.join_btn);
+        email = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.join_edit_email);
+        birth = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.join_edit_birth);
+        name = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.join_edit_name);
+        pw = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.join_edit_pw);
+        pwCheck = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.join_edit_pwCheck);
+        firebaseAuth = firebaseAuth.getInstance();
+        email_layout = (com.google.android.material.textfield.TextInputLayout) findViewById(R.id.join_edit_email_layout);
+        pw_layout = (com.google.android.material.textfield.TextInputLayout) findViewById(R.id.join_edit_pw_layout);
+        pwCheck_layout = (com.google.android.material.textfield.TextInputLayout) findViewById(R.id.join_edit_pwCheck_layout);
+        
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //가입하기 버튼 눌렀을때 실행되는 코드
+            public void onClick(View v) {
+                //공백인 부분을 제거하고 보여주는 trim();
+                final String s_email = email.getText().toString().trim();
+                final String s_pwd = pw.getText().toString().trim();
+                final String s_pwdCheck = pwCheck.getText().toString().trim();
+                final String s_birth = birth.getText().toString().trim();
+                final String s_name = name.getText().toString().trim();
+
+                //비밀번호 확인와 비밀번호가 일치했을 때
+                if(rightEmail&&rightPW&&rightPWCheck) {
+                    firebaseAuth.createUserWithEmailAndPassword(s_email, s_pwd)
+                            .addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(JoinActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        Toast.makeText(JoinActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                }
+                            });
+                }
+                //비밀번호 확인와 비밀번호가 일치하지 않았을 때
+                else{
+                    Toast.makeText(JoinActivity.this, "잘못 입력한 영역이 있는지 확인해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+        });
+
+        //정상적인 이메일 형식인지 확인
+        setupEmailLabelError();
+        //정상적인 비밀번호 형식인지 확인
+        setupPWLabelError();
+        //비밀번호 확인란에 작성한 비밀번호와 비밀번호가 일치한지 확인
+        setupPWCheckLabelError();
     }
+
+    private void setupEmailLabelError() {
+        final TextInputLayout floatingUseLabel = (TextInputLayout) findViewById(R.id.join_edit_email_layout);
+        email_layout.getEditText().addTextChangedListener(new TextWatcher() {
+            // ...
+            @Override
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
+                /*if (text.length() > 0 && text.length() <= 4) {
+                    floatingUsernameLabel.setError("오류");
+                    floatingUsernameLabel.setErrorEnabled(true);
+                } else {
+                    floatingUsernameLabel.setErrorEnabled(false);
+                }*/
+
+                Pattern pattern = android.util.Patterns.EMAIL_ADDRESS;
+
+                if(pattern.matcher(email.getText().toString().trim()).matches()){
+                    //이메일 맞음!
+                    email_layout.setErrorEnabled(false);
+                    email.setBackgroundResource(R.drawable.text_field_background);
+                    rightEmail =true;
+                } else {
+                    //이메일 아님!
+                    email_layout.setError(getText(R.string.join_wrong_email));
+                    email_layout.setErrorEnabled(true);
+                    email.setBackgroundResource(R.drawable.text_field_background_error);
+                    rightEmail =false;
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void setupPWLabelError() {
+        final TextInputLayout floatingUseLabel = (TextInputLayout) findViewById(R.id.join_edit_pw_layout);
+        pw_layout.getEditText().addTextChangedListener(new TextWatcher() {
+            // ...
+            @Override
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
+                if (text.length() > 5 && text.length() <= 20) {
+                    pw_layout.setErrorEnabled(false);
+                    pw.setBackgroundResource(R.drawable.text_field_background);
+                    rightPW=true;
+                }
+                else {
+                    pw_layout.setError(getText(R.string.join_wrong_pw));
+                    pw_layout.setErrorEnabled(true);
+                    pw.setBackgroundResource(R.drawable.text_field_background_error);
+                    rightPW=false;
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void setupPWCheckLabelError() {
+        final TextInputLayout floatingUseLabel = (TextInputLayout) findViewById(R.id.join_edit_pwCheck_layout);
+        pwCheck_layout.getEditText().addTextChangedListener(new TextWatcher() {
+            // ...
+            @Override
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
+                if(pw.getText().toString().trim().equals(pwCheck.getText().toString().trim())){
+                    pwCheck_layout.setErrorEnabled(false);
+                    pwCheck.setBackgroundResource(R.drawable.text_field_background);
+                    rightPWCheck=true;
+                }
+                else{
+                    pwCheck_layout.setError(getText(R.string.join_wrong_pwCheck));
+                    pwCheck_layout.setErrorEnabled(true);
+                    pwCheck.setBackgroundResource(R.drawable.text_field_background_error);
+                    rightPWCheck=false;
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/eustress/moong/MainActivity.java
+++ app/src/main/java/com/eustress/moong/MainActivity.java
@@ -1,16 +1,26 @@
 package com.eustress.moong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login;
+    private com.google.android.material.textfield.TextInputEditText email_main;
+    private com.google.android.material.textfield.TextInputEditText pwd_main;
     private FirebaseAuth mAuth;
 
     @Override
@@ -19,6 +29,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        login = (Button) findViewById(R.id.login_btn);
+        email_main = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edit_id);
+        pwd_main = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.edit_pw);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_main.getText().toString().trim();
+                String pwd = pwd_main.getText().toString().trim();
+                //String형 변수 email.pwd(edittext에서 받오는 값)으로 로그인하는것
+                mAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {//성공했을때
+                                    Intent intent = new Intent(MainActivity.this, CloudActivity.class);
+                                    startActivity(intent);
+                                } else {//실패했을때
+                                    Toast.makeText(MainActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
     @Override
     public void onStart() {
